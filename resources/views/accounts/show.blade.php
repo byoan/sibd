93e2491ba9f831114d1756591b826c7428563f88
Yoan Ballesteros <b.yoan@live.fr>
2018-02-11 22:50:47 +0100

    Update show view to handle json arrays

--- resources/views/accounts/show.blade.php
+++ resources/views/accounts/show.blade.php
@@ -1,6 +1,5 @@
 <h1>Account details</h1>
 
-
 <h2>Account id : {{ $account->id }}</h2>
 <h2>Account balance : {{ $account->balance }}</h2>
 <h2>Account history</h2>
@@ -8,8 +7,8 @@
     @if (empty($account->history))
     <li>No transactions available</li>
     @else
-        @foreach ($account->history as $name => $transaction)
-            <li>{{ $name }} : {{ $transaction }}</li>
+        @foreach ($account->history as $id => $row)
+            <li>{{ $row->transactionName }} : {{ $row->newBalance }}</li>
         @endforeach
     @endif
 </ul>
