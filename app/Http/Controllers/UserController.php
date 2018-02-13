842fae4b2ea396f19ef715cc23dfacf75c94e4ee
Yoan Ballesteros <b.yoan@live.fr>
2018-02-13 23:37:31 +0100

    Add missing use for the UpdateUserRequest

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UpdateUserRequest;
 use Illuminate\Support\Facades\Db;
 
 class UserController extends Controller
