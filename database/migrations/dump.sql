3b2f9edd4000831e46274ac5260385d8abc456b0
Yoan Ballesteros <b.yoan@live.fr>
2018-02-22 16:21:46 +0100

    Update dump.sql to use MyISAM instead of InnoDB

--- database/migrations/dump.sql
+++ database/migrations/dump.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39ba12915ff9ef5f55f5594ecadd3b9555e51cfe7dcb2d0ad328263ac8769696
+oid sha256:869e2a5d44eae880d92b036ac6e16a2b721f3ecb2e5a19ee8bb78c670f89c00e
 size 444999775
