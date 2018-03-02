ae5941b55c7be92cb36d7592995ed565988f442d
Yoan Ballesteros <b.yoan@live.fr>
2018-03-02 16:16:47 +0100

    Update DatabaseSeeder to use all the new seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
         $this->call(AdsTableSeeder::class);
+        $this->call(AdsListTableSeeder::class);
+        $this->call(AttsTableSeeder::class);
         $this->call(AutoTasksTableSeeder::class);
+        $this->call(ContestsTableSeeder::class);
+        $this->call(DiseaseListsTableSeeder::class);
+        $this->call(DiseasesTableSeeder::class);
+        $this->call(HorsesTableSeeder::class);
         $this->call(IndicatorsTableSeeder::class);
         $this->call(InfrastructuresTableSeeder::class);
-        $this->call(DiseasesTableSeeder::class);
+        $this->call(ItemsTableSeeder::class);
+        $this->call(NewspapersTableSeeder::class);
         $this->call(NewsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
