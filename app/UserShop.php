b4bdc6a68526d6fd6458febc5033ba9dc232dd4f
Antoine Sirven <antoinesirvenpro@gmail.com>
2018-03-07 22:10:23 +0100

    UserShop Model

--- app/UserShop.php
+++ app/UserShop.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserShop extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'IdUser','horseList','itemList','infraList','ridingStableList','horseClubList'
+    ];
 }
