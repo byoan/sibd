5e8d1bdcf75c570b675be9d929ccd951a257adde
Antoine Sirven <antoinesirvenpro@gmail.com>
2018-03-07 22:11:30 +0100

    HorseClub Model

--- app/HorseClub.php
+++ app/HorseClub.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class HorseClub extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'capacity','infraList','contestList','userList','idUser'
+    ];
 }
