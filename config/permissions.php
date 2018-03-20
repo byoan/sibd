d1804242567d8d0e2e5e84345ba94888e307dd81
Antoine Sirven <antoinesirvenpro@gmail.com>
2018-03-20 01:23:28 +0100

    - Add permissions

--- config/permissions.php
+++ config/permissions.php
@@ -3,9 +3,31 @@
 return [
     'admin' => '*',
     'moderator' => array(
-        'user' => array('delete', 'update')
+        'user' => array('select', 'delete', 'update')
     ),
     'specialist' => array(
         'horses' => array('select', 'update')
-    )
+    ),
+    'manager' => array(
+        '*' => array('index')
+    ),
+    'autoTask' => array(
+        '*' => array('shutdown', 'reload', 'process')
+    ),
+    'developer' => array(
+        '*' => array('select', 'insert', 'update', 'delete')
+    ),
+    'developer' => array(
+        '*' => array('select', 'insert', 'update', 'delete')
+    ),
+    'contestAdmin' => array(
+        'contest' => array('select', 'insert', 'update', 'delete')
+    ),
+    'editor' => array(
+        'newspaper' => array('select', 'insert', 'update', 'delete')
+    ),
+    'client' => array(
+        'contest' => array('select'),
+        'newspaper' => array('select')
+    ),
 ];
