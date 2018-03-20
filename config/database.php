230a317bac5798e7992aa2ffd03dc1de54c52e8f
JulienS13 <sorelli.julien@gmail.com>
2018-03-20 01:18:57 +0100

    database.php

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE'),
             'username' => 'root',
-            'password' => 'root',
+            'password' => '',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
